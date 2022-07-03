1c25f7909eab8c926c697a835f03c9c6bb232fd2
AntonKolomytsev <96843369+AntonKolomytsev@users.noreply.github.com>
2022-07-04 00:33:37 +0300

    Final version

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 // Create image-container & loader & array
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
+// add consant
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = "FZjCZx1Ryj9piY5bNFtmbbMn-WFqYUnMEiedfLx8gCY";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+//Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
 // Helper function to set attributes on DOM elements
-function setAttributes(element, attributes){
-    for(const key in attributes){
-        element.setAttribute(key, attributes[key]);
-    }
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
 }
 //Create elements for links & photos , add to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
   //Run function for each object in photosArray
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
@@ -23,19 +39,23 @@ function displayPhotos() {
     // item.setAttribute("href", photo.links.html);
     // item.setAttribute("target", "_blank");
     setAttributes(item, {
-        href: photo.links.html,
-        target: '_blank' ,
+      href: photo.links.html,
+      target: "_blank",
     });
+
     // Create <img> for photo
     const img = document.createElement("img");
     // img.setAttribute("src", photo.urls.regular);
     // img.setAttribute("alt", photo.alt_description);
     // img.setAttribute("title", photo.alt_description);
-    setAttributes(img,{
-        src: photo.urls.regular,
-        alt: photo.alt_description,
-        title: photo.alt_description,     
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
     });
+    //Events Listener, check when each is finished loading
+    img.addEventListener("load", imageLoaded);
+
     // Put <img> inside <a>, then put both inside imageContainer element
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -52,5 +72,17 @@ async function getPhotos() {
     //Catch error here
   }
 }
+// Check to see if scrolling near bottom of page, Load More Photos
+
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
+
 // on Load
 getPhotos();
